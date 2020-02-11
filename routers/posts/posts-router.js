5d9c9abdfc40fd4d803a8894c0ef879edc278997
aaronjan <aaronjan98@gmail.com>
2020-02-11 15:18:20 -0800

    made a `POST` request to `/api/posts`

--- routers/posts/posts-router.js
+++ routers/posts/posts-router.js
@@ -4,6 +4,21 @@ const Posts = require('../../data/db.js');
 
 const router = express.Router();
 
+router.post('/', (req, res) => {
+    const { title, contents } = req.body;
+
+    Posts.insert(req.body).then(post => {
+        if(!title || !contents){
+            res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+        }else {
+            res.status(201).json(post);
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "There was an error while saving the post to the database" });
+    });
+})
+
 router.get('/', (req, res) => {
     const pagination = req.query;
     console.log(pagination);
